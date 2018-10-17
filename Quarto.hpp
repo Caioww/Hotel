77662728d5fb0d2449559de044da2a0b13dda6d3
djalma <djalminhajr_cunha@hotmail.com>
2018-10-17 19:09:14 -0300

    agora cria(porem ainda nao salva) os quartos do hotel

--- Quarto.hpp
+++ Quarto.hpp
@@ -16,14 +16,14 @@ using namespace std;
  */
 class Quarto{
 public:
-//a funcao quarto serve apenas para iniciar tudo zerado
+
     //para adcionar quartos, porem os unicos que pode fazer isso é o gerente e o dono
-    void adcionaQuarto(string tipo,string descricao,double valor,int quantidade,int andar){
+    void criarQuarto(string tipo,string descricao,double valor,int quantidade,int andar){
         string code;
-    cin>>code;
-    Senha oi;
+        cin>>code;
+        Senha oi;
         if(oi.confereSenha(code)){
-        FILE* arquivo;
+            FILE* arquivo;
             if ((arquivo = fopen("Caracteristicas_Quartos.txt", "r")) == NULL) {
                 fclose(arquivo);
                 arquivo = fopen("Caracteristicas_Quartos.txt", "w");
@@ -40,8 +40,8 @@ public:
                 fclose(arquivo);
                 arquivo = fopen("Caracteristicas_Quartos.txt", "w");
                 fprintf(arquivo, "%i\n", quant);
-                fprintf(arquivo, "%s\n%s\n%f\n%i\n%i\n%i\n%i\n%i\n", tipo.c_str(), descricao.c_str(), valor,
-                        quantidade,andar,0, true,1,quantidade);
+                fprintf(arquivo, "%s\n%s\n%f\n%i\n%i\n%i\n%i\n", tipo.c_str(), descricao.c_str(), valor,
+                        quantidade,andar,1,(1+quantidade));
                 fclose(arquivo);
                 puts("contato salvo");
             } else {
@@ -52,6 +52,7 @@ public:
                 //esse primeiro fgets serve simplismente para não salvar o numero inicial
                 fgets(sem, 50, arquivo);
                 for (int i = 0; i < quant; i++) {
+                    /*
                     fgets(sem, 50, arquivo);
                     vet[i].tipoQuarto = sem;
                     fgets(sem, 50, arquivo);
@@ -77,24 +78,40 @@ public:
                     fgets(sem, 50, arquivo);
                     dois = stod(sem);
                     vet[i].disponiveis = dois;
+                     */
+                    fgets(sem, 50, arquivo);
+                    vet[i].tipoQuarto = sem;
+                    fgets(sem, 50, arquivo);
+                    vet[i].descricao = sem;
+                    fgets(sem, 50, arquivo);
+                    double ola = stod(sem);
+                    vet[i].valor_diaria = ola;
+                    fgets(sem, 50, arquivo);
+                    int dois = stod(sem);
+                    vet[i].existentes = dois;
+                    fgets(sem, 50, arquivo);
+                    dois = stod(sem);
+                    vet[i].andar = dois;
+                    vet[i].inicial = (vet[(i - 1)].faixa_de_numeros) + 1;
+                    vet[i].faixa_de_numeros = vet[i].inicial + vet[i].existentes;
                 }
-                arquivo = fopen("Senhas.txt", "w");
+                arquivo = fopen("Caracteristicas_Quartos.txt", "w");
                 /*
                  * falta implementar melhor
                  */
                 quant++;
                 fprintf(arquivo, "%i\n", quant);
-                for (int i = 0; i < (quant-1); i++) {
-                    fprintf(arquivo, "%s%s%f\n%i\n%i\n%i\n%i\n%i\n", vet[i].tipoQuarto.c_str(),
+                for (int i = 0; i < (quant - 1); i++) {
+                    fprintf(arquivo, "%s%s%f\n%i\n%i\n%i\n%i\n", vet[i].tipoQuarto.c_str(),
                             vet[i].descricao.c_str(), vet[i].valor_diaria,
-                            vet[i].existentes,vet[i].andar,vet[i].nPessoas,
-                            vet[i].estado,vet[i].numero,vet[i].disponiveis);
+                            vet[i].existentes, vet[i].andar, vet[i].inicial, vet[i].faixa_de_numeros);
                 }
                 fprintf(arquivo, "%s\n%s\n%f\n%i\n%i\n%i\n%i\n%i\n", tipo.c_str(), descricao.c_str(), valor,
-                        quantidade,andar,0, true,1,quantidade);
+                        quantidade, andar, 0, vet[(quant - 1)].faixa_de_numeros + 1,
+                        (vet[(quant - 1)].faixa_de_numeros + 1) + quantidade);
                 fclose(arquivo);
-                puts("contato salvo");
-
+                ocuparQuarto();
+                puts("quarto criado");
             }
         }
         else{
@@ -103,10 +120,57 @@ public:
 
     }
 
-    void ganhapao(){
+    //para criar inicialmente os quartos pré estipulados, inicialmente todos livres
+    void ocuparQuarto(){
+    FILE* arquivo;
+        arquivo = fopen("Caracteristicas_Quartos.txt", "r");
+        char txt[100] = "oi\n";
+        int tamanho;
+        fscanf(arquivo,"%i", &tamanho);
+        caracteristica aux[tamanho];
+        fgets(txt,100,arquivo);
+        for (int i = 0; i < tamanho; i++) {
+            fgets(txt, 100, arquivo);
+            aux[i].tipoQuarto = txt;
+            fgets(txt, 100, arquivo);
+            aux[i].descricao = txt;
+            fgets(txt, 100, arquivo);
+            double ola = stod(txt);
+            aux[i].valor_diaria = ola;
+            fgets(txt, 100, arquivo);
+            int dois = stod(txt);
+            aux[i].existentes = dois;
+            fgets(txt, 100, arquivo);
+            dois = stod(txt);
+            aux[i].andar = dois;
+            aux[i].inicial = (aux[(i - 1)].faixa_de_numeros) + 1;
+            aux[i].faixa_de_numeros = aux[i].inicial + aux[i].existentes;
+        }
+            caracteristica vet[tamanho];
+        for (int j = 0; j < tamanho; ++j) {
+            for (int i = vet[j].inicial; i <=vet[j].faixa_de_numeros; i++) {
+                vet[i].numero = i;
+                vet[i].estado = true;
+                vet[i].valor_diaria = aux[j].valor_diaria;
+                vet[i].tipoQuarto = aux[j].tipoQuarto;
+                vet[i].descricao = aux[j].descricao;
+                vet[i].nPessoas = 0;
+                vet[i].andar = aux[j].andar;
+                vet[i].existentes = aux[j].existentes;
+                vet[i].disponiveis = aux[j].disponiveis;
+                vet[i].nPessoas = NULL;
+            }
+        }
+
+
+        fclose(arquivo);
+
+    }
+
+    //para ocupar um quarto
+    void populaQuarto(int num){
 
 }
-
 
 private:
     struct caracteristica {
@@ -120,8 +184,53 @@ private:
         //Fazer um List que armazena os quartos cadastrados,
         int disponiveis;
         int existentes;
+        int inicial;
+        int faixa_de_numeros;
     };
     typedef struct caracteristica caracteristica;
+
+    caracteristica** matriz;
+    void fazVetor(caracteristica* aux, caracteristica* vet){
+        FILE* arquivo;
+        arquivo = fopen("Caracteristicas_Quartos.txt", "r");
+        char txt[100] = "oi\n";
+        int tamanho;
+        fgets(txt,100,arquivo);
+        fscanf(arquivo,"%i", &tamanho);
+        fgets(txt,100,arquivo);
+        for (int i = 0; i < tamanho; i++) {
+            fgets(txt, 100, arquivo);
+            aux[i].tipoQuarto = txt;
+            fgets(txt, 100, arquivo);
+            aux[i].descricao = txt;
+            fgets(txt, 100, arquivo);
+            double ola = stod(txt);
+            aux[i].valor_diaria = ola;
+            fgets(txt, 100, arquivo);
+            int dois = stod(txt);
+            aux[i].existentes = dois;
+            fgets(txt, 100, arquivo);
+            dois = stod(txt);
+            aux[i].andar = dois;
+            aux[i].inicial = (aux[(i - 1)].faixa_de_numeros) + 1;
+            aux[i].faixa_de_numeros = aux[i].inicial + aux[i].existentes;
+        }
+        for (int j = 0; j < tamanho; ++j) {
+            for (int i = vet[j].inicial; i <=vet[j].faixa_de_numeros; i++) {
+                vet[i].numero = i;
+                vet[i].estado = true;
+                vet[i].valor_diaria = aux[j].valor_diaria;
+                vet[i].tipoQuarto = aux[j].tipoQuarto;
+                vet[i].descricao = aux[j].descricao;
+                vet[i].nPessoas = 0;
+                vet[i].andar = aux[j].andar;
+                vet[i].existentes = aux[j].existentes;
+                vet[i].disponiveis = aux[j].disponiveis;
+                vet[i].nPessoas = NULL;
+            }
+        }
+        fclose(arquivo);
+    }
 
 };
 
