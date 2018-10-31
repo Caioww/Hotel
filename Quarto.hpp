1e87ce349bcb21fccb4efd3bf69fad9523532ae8
djalma <djalminhajr_cunha@hotmail.com>
2018-10-31 16:35:18 -0300

    pilha ta ok

--- Quarto.hpp
+++ Quarto.hpp
@@ -1,7 +1,3 @@
-//
-// Created by djalma cunha on 16/09/2018.
-//
-
 #ifndef HOTEL_QUARTO_HPP
 #define HOTEL_QUARTO_HPP
 #include <iostream>
@@ -10,17 +6,8 @@
 #include "Senha.hpp"
 #include "Caracteristicas.hpp"
 using namespace std;
-/**
- * melhor colocar o criar quartos nas configuracoes ou seja num arquivo a parte
- * por isso esta bugando
- */
 class Quarto{
 public:
-
-    //para adcionar quartos, porem os unicos que pode fazer isso é o gerente e o dono
-    void criarQuarto(string tipo,string descricao,double valor,int quantidade,int andar){
-       //prato.empilha(0,0,true,tipo,descricao,0,andar,quantidade,quantidade,0,0);
-    }
 
     //para criar inicialmente os quartos pré estipulados, inicialmente todos livres
     void ocuparQuarto(int numero,int pessoas,string tipo,string descricao,double valor,int quantidade,int andar){
@@ -32,67 +19,49 @@ public:
         auxiliar.existentes = quantidade;
         auxiliar.andar = andar;
         prato.empilha(auxiliar);
+        quant++;
     }
 
-
-
-    //esta imprimindo apenas 1
-    void imprime(){
-        Pilha q;
+    void buscaQuarto(int numero){
         int l = 0;
-        while (prato.desemplilha(&auxiliar) && l < 3){
-            prato.empilha(auxiliar);
-            cout<<auxiliar.descricao<<endl;
+        while (l < quant){
+            //nao inverter os valores
+            prato.busca(numero,l);
             l++;
         }
     }
 
-private:
-    
-    Pilha prato;
-    Caracteristicas auxiliar;
-
-    void fazVetor(Caracteristicas* aux, Caracteristicas* vet){
-        FILE* arquivo;
-        arquivo = fopen("Caracteristicas_Quartos.txt", "r");
-        char txt[100] = "oi\n";
-        int tamanho;
-        fgets(txt,100,arquivo);
-        fscanf(arquivo,"%i", &tamanho);
-        fgets(txt,100,arquivo);
-        for (int i = 0; i < tamanho; i++) {
-            fgets(txt, 100, arquivo);
-            aux[i].tipoQuarto = txt;
-            fgets(txt, 100, arquivo);
-            aux[i].descricao = txt;
-            fgets(txt, 100, arquivo);
-            double ola = stod(txt);
-            aux[i].valor_diaria = ola;
-            fgets(txt, 100, arquivo);
-            int dois = stod(txt);
-            aux[i].existentes = dois;
-            fgets(txt, 100, arquivo);
-            dois = stod(txt);
-            aux[i].andar = dois;
-            aux[i].inicial = (aux[(i - 1)].faixa_de_numeros) + 1;
-            aux[i].faixa_de_numeros = aux[i].inicial + aux[i].existentes;
+    //esta imprimindo apenas 1
+    void imprime(){
+        //bolar esquema para descobrir quantidade de quartos
+        int l = 0;
+        while (l < quant){
+            prato.imprime(l);
+            l++;
         }
-        for (int j = 0; j < tamanho; ++j) {
-            for (int i = vet[j].inicial; i <=vet[j].faixa_de_numeros; i++) {
-                vet[i].numero = i;
-                vet[i].estado = true;
-                vet[i].valor_diaria = aux[j].valor_diaria;
-                vet[i].tipoQuarto = aux[j].tipoQuarto;
-                vet[i].descricao = aux[j].descricao;
-                vet[i].nPessoas = 0;
-                vet[i].andar = aux[j].andar;
-                vet[i].existentes = aux[j].existentes;
-                vet[i].disponiveis = aux[j].disponiveis;
-                vet[i].nPessoas = NULL;
+    }
+
+    void remove1(int numero){
+        Caracteristicas temp[quant];
+        int init = quant;
+        for (int k = 0; k < quant; k++) {
+            prato.copia(numero,k,temp,quant);
+        }
+        for (int i = 0; i < init; i++) {
+            if(prato.remove(numero,i,temp,quant)){
+                quant = init -1;
             }
         }
-        fclose(arquivo);
+        prato.topo = 0;
+        for (int j = 0; j < quant; j++) {
+            prato.empilha(temp[j]);
+        }
     }
+
+private:
+    int quant = 0;
+    Pilha prato;
+    Caracteristicas auxiliar;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@
 #include "Quarto.hpp"
 int main() {
     Quarto oi;
-    oi.ocuparQuarto(200,3,"top","top",2,9,2);
-    oi.ocuparQuarto(1,1,"bosta","bosta",1,1,1);
-    oi.ocuparQuarto(200,3,"top","top",2,9,2);
+    oi.ocuparQuarto(1,1,"top","top",1,1,1);
+    oi.ocuparQuarto(2,2,"bosta","bosta",2,2,2);
+    oi.ocuparQuarto(3,3,"top","tooooop",3,3,3);
+    oi.ocuparQuarto(4,4,"top","4444",4,4,4);
+    oi.ocuparQuarto(5,5,"bosta","55555",5,5,5);
+    oi.ocuparQuarto(6,6,"top","6666666",6,6,6);
+    oi.imprime();
+    oi.remove1(3);
     oi.imprime();
     return 0;
 }
